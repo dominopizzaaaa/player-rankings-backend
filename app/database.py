8596e5a448090334171f8c4ee8604494006c50d3
dominopizzaaaa <domksj23@gmail.com>
2025-03-10 20:10:07 +0800

    Add Docker file

--- app/database.py
+++ app/database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+pymysql://elo_user:dominopizza@localhost/player_rankings"
+DATABASE_URL = "mysql+pymysql://root:%40xbqpassword1@35.240.170.40/player_rankings"
+
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
